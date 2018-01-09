8053b9e15765c4c4c6e2353342a0d6ba5a10466a
selwb <baiyuerx@gmail.com>
2018-01-09 16:04:34 +0800

    linear regression implememted with gradient descent

--- Linear_Regression/ex1_linreg.py
+++ Linear_Regression/ex1_linreg.py
@@ -23,7 +23,7 @@ def train_with_sklearn(train_x, train_y):
     return linreg.coef_
 
 
-def train_batch(train_x, train_y, alpha,epoch):
+def train_batch(train_x, train_y, alpha, epoch):
     m_, n_ = train_x.shape
     theta = np.zeros((n_, 1))
     cost_data = [cost(theta, train_x, train_y)]
@@ -32,11 +32,12 @@ def train_batch(train_x, train_y, alpha,epoch):
         predicted_y = np.dot(train_x, theta)
         loss = predicted_y - train_y
         # old_theta = theta
-        theta = theta - alpha * (np.dot(loss.T, train_x)).T/m_
+        theta = theta - alpha * (np.dot(loss.T, train_x)).T / m_
         count += 1
         cost_data.append(cost(theta, train_x, train_y))
         if count % epoch == 0:
             return theta, cost_data
+
 
 def train_stochastic(train_x, train_y, alpha, epoch):
     m_, n_ = train_x.shape
@@ -48,28 +49,54 @@ def train_stochastic(train_x, train_y, alpha, epoch):
             predicted_y = np.dot(train_x[i], theta)
             loss = np.asscalar(predicted_y - train_y[i])
             # old_theta = theta
-            theta = theta - alpha * loss * train_x[i].reshape((14,1))/m_
+            theta = theta - alpha * loss * train_x[i].reshape((14, 1)) / m_
         count += 1
-        cost_data.append(cost(theta,train_x,train_y))
+        cost_data.append(cost(theta, train_x, train_y))
         if count % epoch == 0:
             return theta, cost_data
 
 
-def train_tf(train_x, train_y, alpha, epoch, Optimizer=tf.train.GradientDescentOptimizer):
+def train_tf(train_x, train_y, alpha, epoch, optimizer=tf.train.GradientDescentOptimizer):
+    X = tf.placeholder(tf.float64, shape=train_x.shape)
+    Y = tf.placeholder(tf.float64, shape=train_y.shape)
 
-def cost(theata,X,Y):
+    with tf.variable_scope('linear_regression'):
+        theta = tf.get_variable('theta', (X.shape[1], 1), initializer=tf.constant_initializer(), dtype=tf.float64)
+        y_predicted = tf.matmul(X, theta)
+        loss = 1 / (2 * len(train_x)) * tf.matmul((y_predicted - Y), (y_predicted - Y), transpose_a=True)
+
+    opt = optimizer(learning_rate=alpha)
+    opt_operation = opt.minimize(loss)
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        loss_data = []
+        for i in range(epoch):
+            _, loss_val, theta_tf = sess.run([opt_operation, loss, theta], feed_dict={X: train_x, Y: train_y})
+            loss_data.append(loss_val[0, 0])
+
+            if len(loss_data) > 1 and np.abs(
+                    loss_data[-1] - loss_data[-2]) < 10 ** -9:  # early break when it's converged
+                # print('Converged at epoch {}'.format(i))
+                break
+
+    tf.reset_default_graph()
+    return theta_tf, loss_data
+
+
+def cost(theata, X, Y):
     loss = np.dot(X, theata) - Y
     m = X.shape[0]
-    cost = 1/(2 * m ) * np.sum(np.square(loss))
+    cost = 1 / (2 * m) * np.sum(np.square(loss))
     return cost
 
 
 if __name__ == "__main__":
     file_path = "/home/selwb/Document/Machine_learning/UFLDL/stanford_dl_ex-master/ex1/housing.data"
     train_X, test_X, train_Y, test_Y, m, n = load_data(file_path)
-    theta_sto, cost_sto= train_stochastic(train_X, train_Y, 0.0001, 1000)
+    theta_sto, cost_sto = train_tf(train_X, train_Y, 0.000001, 1000000)
     # sk_theta = np.array(train_with_sklearn(train_X, train_Y)).T
     #
     # loss1 = np.dot(test_X, sk_theta) - test_Y
     print(theta_sto)
-    print(cost_sto)
+    print(min(cost_sto))
