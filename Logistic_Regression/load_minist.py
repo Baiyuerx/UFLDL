a2b939e03355d52dae67d7f9b4e1d6c59c66a282
baiyuerx <baiyuerx@gmail.com>
2018-01-09 23:06:28 +0800

    Load Minist data

--- Logistic_Regression/load_minist.py
+++ Logistic_Regression/load_minist.py
@@ -8,15 +8,15 @@ def load_minist_images(filepath):
     with gzip.open(filepath, 'rb') as f:
         filebuf = f.read()
     offset = 0
-    magic_number, image_numbs, row_numbs, col_numbs = struct.unpack_from('>4I', filebuf, offset)
-    offset = offset + struct.calcsize('4I')
+    magic_number, image_numbs, row_numbs, col_numbs = struct.unpack_from('>IIII', filebuf, offset)
+    offset = offset + struct.calcsize('>IIII')
     images = []
     for i in range(image_numbs):
         image_values = struct.unpack_from('>784B', filebuf, offset)
-        offset = offset + struct.calcsize('>4I')
+        offset = offset + struct.calcsize('>784B')
         image_values = list(image_values)
         images.append(image_values)
-    return np.array(images)
+    return np.array(images, dtype='uint8')
 
 
 def load_minist_labels(filepath):
@@ -30,8 +30,8 @@ def load_minist_labels(filepath):
         label_value = struct.unpack_from('>B', filebuf, offset)
         offset = offset + struct.calcsize('>B')
         labels.append(list(label_value))
-    return np.array(labels)
+    return np.array(labels, dtype='uint8')
 
 if __name__ == '__main__':
-    print(load_minist_images('E:\\Notes\\ufldl\\Data\\ex1\\train-images-idx3-ubyte.gz')[4])
-    print(load_minist_labels('E:\\Notes\\ufldl\\Data\\ex1\\train-labels-idx1-ubyte.gz')[4])
+    print(load_minist_images('E:\\Notes\\ufldl\\Data\\ex1\\train-images-idx3-ubyte.gz').shape)
+    print(load_minist_labels('E:\\Notes\\ufldl\\Data\\ex1\\train-labels-idx1-ubyte.gz').shape)
