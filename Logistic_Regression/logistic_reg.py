06fcf1749dc0e22d94bdf222a44feb4f587d7fc6
selwb <baiyuerx@gmail.com>
2018-01-10 18:04:40 +0800

    implement of logistic regression

--- Logistic_Regression/logistic_reg.py
+++ Logistic_Regression/logistic_reg.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 from sklearn.linear_model import LogisticRegression
+import tensorflow as tf
 
 
 def sigmod(x):
@@ -29,6 +30,28 @@ def logstic_gradient_sto(train_x, train_y, epoch=1000, alpha=0.001):
     #         loss =  train_y[i] - np.exp(np.dot(train_x[i], theta)) / sigmod(np.dot(train_x[i], theta))
     #         theta = theta + alpha * np.dot(train_x[i], loss.reshape(-1, 1))
     return theta
+
+
+def logistic_tf(train_x, train_y, epoch=1000, alpha=0.001, batch_size = 100):
+    X = tf.placeholder(tf.float64, (None, train_x.shape[1]))
+    Y = tf.placeholder(tf.float64, (None, 2))
+
+    W = tf.get_variable((784, 2), initializer=tf.constant_initializer())
+    b = tf.get_variable(10, initializer=tf.constant_initializer())
+
+    predicted = tf.nn.softmax(tf.matmul(X, W) + b)
+
+    cost = tf.reduce_mean(-tf.reduce_sum(Y*tf.log(predicted)), reduction_indices=1)
+
+    optimazer = tf.train.GradientDescentOptimizer(alpha).minimize(cost)
+
+    init = tf.global_variables_initializer()
+
+    with tf.Session as sess:
+        sess.run(init)
+        for e in range(epoch):
+            avg_cost = 0
+
 
 
 def logistic_sk(train_x, train_y):
