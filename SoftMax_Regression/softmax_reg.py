fdb15bdd9b5358e8a5b25603aa79bc251ee7d0c2
selwb <baiyuerx@gmail.com>
2018-01-14 15:54:56 +0800

    implement of softmax regression

--- SoftMax_Regression/softmax_reg.py
+++ SoftMax_Regression/softmax_reg.py
@@ -9,6 +9,14 @@ def hypothesis(theta, x):
     predicted_y = np.argmax(probability, axis=1)
     return probability, predicted_y
 
+def dense_to_one_hot(labels_dense, num_classes):
+  """Convert class labels from scalars to one-hot vectors."""
+  num_labels = labels_dense.shape[0]
+  index_offset = np.arange(num_labels) * num_classes
+  labels_one_hot = np.zeros((num_labels, num_classes))
+  labels_one_hot.flat[index_offset + labels_dense.ravel()] = 1
+  return labels_one_hot
+
 
 def softmax_train(x, y, epoch=1000, alpha=0.001, weight_decay_rate=0.01):
     label_cnts = len(np.unique(y))
@@ -23,7 +31,8 @@ def softmax_train(x, y, epoch=1000, alpha=0.001, weight_decay_rate=0.01):
             theta_[:, [i]] = np.sum(x * (y_ - prob[:,[i]]), axis=0).reshape(-1, 1)
         theta = theta + alpha * 1/m * theta_ - weight_decay_rate * theta
         epoch_n = epoch_n + 1
-        print("have trained %d times"%epoch_n)
+        if epoch_n % 100 == 0:
+            print("have trained %d times"%epoch_n)
     return theta
 
 
