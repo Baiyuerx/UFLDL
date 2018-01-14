ed44a0a14a191af040efa99c70547ace75ca5dd6
selwb <baiyuerx@gmail.com>
2018-01-14 15:41:06 +0800

    implement of softmax regression

--- SoftMax_Regression/softmax_reg.py
+++ SoftMax_Regression/softmax_reg.py
@@ -12,8 +12,9 @@ def hypothesis(theta, x):
 
 def softmax_train(x, y, epoch=1000, alpha=0.001, weight_decay_rate=0.01):
     label_cnts = len(np.unique(y))
-    theta = np.ones((x.shape[1], label_cnts))
+    theta = np.random.normal(0, 0.01, (x.shape[1], label_cnts))
     m = y.shape[0]
+    epoch_n = 0
     for e in range(epoch):
         prob, pred_y = hypothesis(theta, x)
         theta_ = np.empty_like(theta)
@@ -21,7 +22,16 @@ def softmax_train(x, y, epoch=1000, alpha=0.001, weight_decay_rate=0.01):
             y_ = (y == i).astype(int)
             theta_[:, [i]] = np.sum(x * (y_ - prob[:,[i]]), axis=0).reshape(-1, 1)
         theta = theta + alpha * 1/m * theta_ - weight_decay_rate * theta
+        epoch_n = epoch_n + 1
+        print("have trained %d times"%epoch_n)
     return theta
+
+
+def accuracy(theta, x, y):
+    predicted = np.argmax(np.dot(x, theta), axis=1)
+    corrected = (predicted == y.ravel()).astype(int)
+    return np.mean(corrected)
+
 
 if __name__ == '__main__':
     x = (np.random.rand(5,3)*10).astype(int)
